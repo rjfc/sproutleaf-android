94a111d0080f1e6ae9dc6ddeb1684a8725447d7d
rjc <russelljfchan@gmail.com>
2019-07-29 02:51:40 -0400

    Added authentication page

---
+++ app/src/main/java/com/example/sproutleaf/AuthenticationActivity.java
@@ -0,0 +1,19 @@
+package com.example.sproutleaf;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+}
--- app/src/main/java/com/example/sproutleaf/MainActivity.java
+++ app/src/main/java/com/example/sproutleaf/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.sproutleaf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI (FirebaseUser user) {
+        if (user == null) {
+            Intent intent = new Intent(this, AuthenticationActivity.class);
+            startActivity(intent);
+        }
     }
 }
