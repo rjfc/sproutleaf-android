e016bc37844ce14e858cdd0bf306213d7715fd6e
rjc <russelljfchan@gmail.com>
2019-08-04 01:43:11 -0400

    Added content to activity_main

--- app/src/main/java/com/example/sproutleaf/MainActivity.java
+++ app/src/main/java/com/example/sproutleaf/MainActivity.java
@@ -6,7 +6,9 @@ import androidx.fragment.app.FragmentManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -26,12 +28,22 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser user = mAuth.getCurrentUser();
         String name = user.getDisplayName();
         if (name == null) {
-            showEditDialog();
+            showGiveNameDialog();
         }
     }
-    private void showEditDialog() {
+
+    // Show give name dialog
+    private void showGiveNameDialog() {
         FragmentManager fm = getSupportFragmentManager();
         GiveNameDialogFragment editNameDialogFragment = GiveNameDialogFragment.newInstance("Provide Your Name");
         editNameDialogFragment.show(fm, "fragment_give_name");
+    }
+
+    public void capturePlant (View view) {
+        final int REQUEST_IMAGE_CAPTURE = 1;
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
